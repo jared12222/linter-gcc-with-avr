65ff6b07734537b45a040e773ec431c350fe9a77
mickey9910326 <mickey9910326@gmail.com>
2017-08-29 17:27:30 +0800

    init

---
+++ spec/linter-gcc-with-avr-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import LinterGccWithAvrView from '../lib/linter-gcc-with-avr-view';
+
+describe('LinterGccWithAvrView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
